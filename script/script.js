90cdd928ac7b1a568ced09576e0f9808ef2a491a
Inoc Mark <128759487+InocIran@users.noreply.github.com>
2023-12-23 17:12:17 +0800

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,105 @@
+function toggleLike() {
+  const button = document.querySelector('.like-button');
+  button.classList.toggle('liked');
+}
+function toggleHeart() {
+  const button = document.querySelector('.heart-button');
+  button.classList.toggle('hearted');
+}
+function toggleAngry() {
+  const button = document.querySelector('.angry-button');
+  button.classList.toggle('angred');
+}
+function toggleLaugh() {
+  const button = document.querySelector('.laugh-button');
+  button.classList.toggle('laughed');
+}
+
+
+function toggleLike1() {
+  const button = document.querySelector('.like-button1');
+  button.classList.toggle('liked1');
+}
+function toggleHeart1() {
+  const button = document.querySelector('.heart-button1');
+  button.classList.toggle('hearted1');
+}
+function toggleAngry1() {
+  const button = document.querySelector('.angry-button1');
+  button.classList.toggle('angred1');
+}
+function toggleLaugh1() {
+  const button = document.querySelector('.laugh-button1');
+  button.classList.toggle('laughed1');
+}
+
+
+function toggleLike2() {
+  const button = document.querySelector('.like-button2');
+  button.classList.toggle('liked2');
+}
+function toggleHeart2() {
+  const button = document.querySelector('.heart-button2');
+  button.classList.toggle('hearted2');
+}
+function toggleAngry2() {
+  const button = document.querySelector('.angry-button2');
+  button.classList.toggle('angred2');
+}
+function toggleLaugh2() {
+  const button = document.querySelector('.laugh-button2');
+  button.classList.toggle('laughed2');
+}
+
+
+function toggleLike3() {
+  const button = document.querySelector('.like-button3');
+  button.classList.toggle('liked3');
+}
+function toggleHeart3() {
+  const button = document.querySelector('.heart-button3');
+  button.classList.toggle('hearted3');
+}
+function toggleAngry3() {
+  const button = document.querySelector('.angry-button3');
+  button.classList.toggle('angred3');
+}
+function toggleLaugh3() {
+  const button = document.querySelector('.laugh-button3');
+  button.classList.toggle('laughed3');
+}
+
+function toggleLike4() {
+  const button = document.querySelector('.like-button4');
+  button.classList.toggle('liked4');
+}
+function toggleHeart4() {
+  const button = document.querySelector('.heart-button4');
+  button.classList.toggle('hearted4');
+}
+function toggleAngry4() {
+  const button = document.querySelector('.angry-button4');
+  button.classList.toggle('angred4');
+}
+function toggleLaugh4() {
+  const button = document.querySelector('.laugh-button4');
+  button.classList.toggle('laughed4');
+}
+
+
+function toggleLike5() {
+  const button = document.querySelector('.like-button5');
+  button.classList.toggle('liked5');
+}
+function toggleHeart5() {
+  const button = document.querySelector('.heart-button5');
+  button.classList.toggle('hearted5');
+}
+function toggleAngry5() {
+  const button = document.querySelector('.angry-button5');
+  button.classList.toggle('angred5');
+}
+function toggleLaugh5() {
+  const button = document.querySelector('.laugh-button5');
+  button.classList.toggle('laughed5');
+}
